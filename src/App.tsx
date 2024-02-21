7b864e65c4c314b6127ee9bd6a48c2bb25c72ddd
Max Hertel <maxherteljr@gmail.com>
2024-02-21 18:44:03 -0300

    fianlizando a conexão

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+import MessagesInput from './MessagesInput';
+import Messages from './Messages';
 
-function App() {
+const App: React.FC = () => {
+  const [socket, setSocket] = useState<Socket | undefined>();
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const send = (value: string) => {
+    socket?.emit('message', value);
+  };
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:8001');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect(); // Desconecta o socket ao desmontar o componente
+    };
+  }, []);
+
+  useEffect(() => {
+    const messageListener = (message: string) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    socket?.on('message', messageListener);
+
+    return () => {
+      socket?.off('message', messageListener); // Remove o ouvinte de eventos ao desmontar o componente
+    };
+  }, [socket]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <MessagesInput send={send} />
+      <Messages messages={messages} />
     </div>
   );
-}
+};
 
 export default App;
